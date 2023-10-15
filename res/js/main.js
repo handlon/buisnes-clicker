41235e5d0666f1bd62fe186882111bc71e52a13b
handlon <jan.handlik@gmail.com>
2023-10-15 17:47:17 +0200

    desing a funkcnost

--- res/js/main.js
+++ res/js/main.js
@@ -2,7 +2,12 @@ const baller=document.getElementById("baller");
 const clickUpgrade=document.getElementById("clikupgrade");
 const autoClicker = document.getElementById("autockicker");
 const clickUpgradeCostDisplay = document.getElementById("displaycostclickupgrade");
-
+const autoUpgradeCostDisplay= document.getElementById("displaycostautoupgrade");
+const clickUpgradeLvl=document.getElementById("clickupgradelvl");
+const autoUpgradeLvl=document.getElementById("autoupgradelvl");
+const barakUpgradeLvl=document.getElementById("barakupgradelvl"); //need dodelat
+const barak= document.getElementById("buisness")//need dodelat
+const barakCostDisplay=document.getElementById("displaycostbarak")//need dodelat
 
 
 let pocet=document.getElementById("pocet");
@@ -12,7 +17,10 @@ let clickUpgradeCost = 100;
 let autoClickerCost = 500;
 let bruhinterval;
 let autockickerIncrease = 0;
-
+let clickUpgradeLvlCounter = 0;
+let autoUpgradeLvlCounter = 0;
+let barakUpgradeLvlCounter = 0; //need dodelat
+let barakCost = 5000;//need dodelat
 
 
 
@@ -37,7 +45,9 @@ clickUpgrade.onclick = () => {
     if (pocetPenez >= clickUpgradeCost) {
         pocetPenez -= clickUpgradeCost;
         clickUpgradeCost += 100;
-
+        clickUpgradeLvlCounter +=1;
+        clickUpgradeLvl.innerHTML="lvl: "+ clickUpgradeLvlCounter;
+        
         pocetPenezZaClick += 5;
         pocet.innerHTML="" + pocetPenez;
         console.log(pocetPenez);
@@ -51,6 +61,9 @@ autoClicker.onclick = () => {
     if (pocetPenez >= autoClickerCost) {
         pocetPenez -= autoClickerCost;
         autoClickerCost += 100;
+        autoUpgradeCostDisplay.innerHTML="autoclicker upgrade cost: "+autoClickerCost;
+        autoUpgradeLvlCounter +=1;
+        autoUpgradeLvl.innerHTML="lvl: "+autoUpgradeLvlCounter;
 
         autockickerIncrease += 20;
         pocet.innerHTML=" " + pocetPenez;
@@ -61,6 +74,25 @@ autoClicker.onclick = () => {
         },1000);
 
     }
+}
+
+barak.onclick = () => {
+    if( pocetPenez >= barakCost) {
+
+        pocetPenez -= barakCost;
+        barakCost += 5000;
+        barakCostDisplay.innerHTML="barak upgrade cost: "+barakCost;
+        barakUpgradeLvlCounter +=1;
+        barakUpgradeLvl.innerHTML="lvl: "+ barakUpgradeLvlCounter;
+        pocet.innerHTML=" " + pocetPenez;
+    
+    }
+}
+
+function cheats2(){
+    pocetPenez += 5000;
+    pocet.innerHTML=" " + pocetPenez;
+    console.log(pocetPenez);
 }
 
 
